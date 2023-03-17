7d9520893a23bd6ac5ab4814b29679f9c9c463b7
Daniel Underhay <27876907+dunderhay@users.noreply.github.com>
2023-03-17 11:15:15 +1100

    changes to python client

--- software/python-client/run.py
+++ software/python-client/run.py
@@ -1,17 +1,23 @@
 import socket
 
-tuskaddr = '78:E3:6D:18:F5:DA'
+tuskaddr = "78:E3:6D:18:F5:DA"
 port = 1 
-size = 4096
+size = 1024
+data = b''
 
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 s.connect((tuskaddr, port))
 
 try:
-    while 1:
-        data = s.recv(size)
-        if data:
-            print(data)
+    while True:        
+        packet = s.recv(size)
+        if len(packet) <= 0:
+            break
+        data += packet
+        if len(data) > 60:
+            print(f"[+] {data.decode()}")
+            data = b''
+        
 except:	
     print("Closing socket")	
     s.close()
