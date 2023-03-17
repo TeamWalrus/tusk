07881510a016ff7b93c21abab8960e038d8dc43c
Daniel Underhay <27876907+dunderhay@users.noreply.github.com>
2023-03-17 14:12:54 +1100

    changes to python client

--- software/python-client/run.py
+++ software/python-client/run.py
@@ -5,8 +5,13 @@ port = 1
 size = 1024
 data = b''
 
-s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.connect((tuskaddr, port))
+try:
+    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    s.connect((tuskaddr, port))
+
+except socket.error as serr:
+    print(f"[!] Socket Error: {serr}")
+    exit(1)
 
 try:
     while True:        
@@ -14,10 +19,12 @@ try:
         if len(packet) <= 0:
             break
         data += packet
+        # if there is at least some card data, let's print it.
+        # //todo could be a better way to do this
         if len(data) > 60:
             print(f"[+] {data.decode()}")
             data = b''
         
 except:	
-    print("Closing socket")	
+    print("[*] Closing socket")	
     s.close()
