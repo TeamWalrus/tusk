35f3d85676a38915ccd89a23d614c230751ba447
Daniel Underhay <27876907+dunderhay@users.noreply.github.com>
2023-06-07 12:50:32 +1000

    update packages (fixes #37)

--- firmware/interface/src/components/DataTable.jsx
+++ firmware/interface/src/components/DataTable.jsx
@@ -7,8 +7,9 @@ export default function DataTable({ filter }) {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
 
-  const streamerr = (e) => {
-    setError(e.message);
+  const streamerr = (error) => {
+    setError("Failed to fetch card data.");
+    console.log(error.message);
   };
 
   const getCardData = async () => {
@@ -34,7 +35,8 @@ export default function DataTable({ filter }) {
           });
       })
       .catch((error) => {
-        setError(error.message);
+        setError("Failed to fetch card data.");
+        console.log(error.message);
       });
     setIsLoading(false);
   };
@@ -84,23 +86,21 @@ export default function DataTable({ filter }) {
   const renderNoCardData = (
     <div className="flex justify-center items-center pt-6">
       <div className="text-center">
-        <div className="alert alert-info shadow-lg">
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="stroke-current flex-shrink-0 w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
-            <span>No card data found.</span>
-          </div>
+        <div className="alert alert-info">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-current shrink-0 w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>No card data found.</span>
         </div>
       </div>
     </div>
@@ -109,23 +109,21 @@ export default function DataTable({ filter }) {
   const renderError = (
     <div className="flex justify-center items-center pt-6">
       <div className="text-center">
-        <div className="alert alert-error shadow-lg">
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current flex-shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>{error}</span>
-          </div>
+        <div className="alert alert-error">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>{error}.</span>
         </div>
       </div>
     </div>
--- firmware/interface/src/views/Settings.jsx
+++ firmware/interface/src/views/Settings.jsx
@@ -118,23 +118,21 @@ export default function Settings() {
   const renderError = (
     <div className="flex justify-center items-center pt-6">
       <div className="text-center">
-        <div className="alert alert-error shadow-lg">
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current flex-shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>{error}</span>
-          </div>
+        <div className="alert alert-error">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>{error}.</span>
         </div>
       </div>
     </div>
@@ -280,7 +278,6 @@ export default function Settings() {
                   </div>
                 </div>
               </div>
-
             </div>
             <div
               id="tab_littlefsinfo"
