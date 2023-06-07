101f942edb20f1e9aa4dcf1ae553d508411ef566
Daniel Underhay <27876907+dunderhay@users.noreply.github.com>
2023-06-07 13:00:39 +1000

    update error message

--- firmware/interface/src/components/DataTable.jsx
+++ firmware/interface/src/components/DataTable.jsx
@@ -8,7 +8,7 @@ export default function DataTable({ filter }) {
   const [error, setError] = useState("");
 
   const streamerr = (error) => {
-    setError("Failed to fetch card data.");
+    setError("Failed to fetch card data - check console log 👀");
     console.log(error.message);
   };
 
@@ -35,7 +35,7 @@ export default function DataTable({ filter }) {
           });
       })
       .catch((error) => {
-        setError("Failed to fetch card data.");
+        setError("Failed to fetch card data - check console log 👀");
         console.log(error.message);
       });
     setIsLoading(false);
@@ -123,7 +123,7 @@ export default function DataTable({ filter }) {
               d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
-          <span>{error}.</span>
+          <span>{error}</span>
         </div>
       </div>
     </div>
