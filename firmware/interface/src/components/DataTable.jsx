ae2ef3307625030b63edde898222aa4197b846c3
Daniel Underhay <27876907+dunderhay@users.noreply.github.com>
2023-06-18 13:40:57 +1000

    reformatting and added cardData and filter to the useMemo hook

--- firmware/interface/src/components/DataTable.jsx
+++ firmware/interface/src/components/DataTable.jsx
@@ -5,9 +5,8 @@ import ErrorAlert from "./ErrorAlert";
 import hidLogo from "../assets/hid_logo.png";
 import gallagherLogo from "../assets/gallagher_logo.png";
 
-
 export default function DataTable({ filter }) {
-  const [cardData, setCardCata] = useState([]);
+  const [cardData, setCardData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
 
@@ -35,7 +34,7 @@ export default function DataTable({ filter }) {
             return reader.read().then(processValue, streamerr);
           }, streamerr)
           .then((data) => {
-            setCardCata([...cardData, ...cardEntry]);
+            setCardData([...cardData, ...cardEntry]);
           });
       })
       .catch((error) => {
@@ -57,58 +56,115 @@ export default function DataTable({ filter }) {
   const renderCardTypeImage = (cardType) => {
     switch (cardType) {
       case "hid":
-        return <img class="h-auto max-w-xs mx-auto" width="80px" src={hidLogo} alt="hid-logo" />;
+        return (
+          <img
+            class="h-auto max-w-xs mx-auto"
+            width="80px"
+            src={hidLogo}
+            alt="hid-logo"
+          />
+        );
       case "gallagher":
-        return <img class="h-auto max-w-xs mx-auto" width="120px" src={gallagherLogo} alt="gallagher-logo" />;
+        return (
+          <img
+            class="h-auto max-w-xs mx-auto"
+            width="120px"
+            src={gallagherLogo}
+            alt="gallagher-logo"
+          />
+        );
     }
   };
 
-  const filteredCardData = useMemo(() =>
-    cardData.filter((card) => {
-      return card.card_number.toString().includes(Object.values({ filter }));
-    })
+  //const filteredCardData = useMemo(() =>
+  //  cardData.filter((card) => {
+  //    return card.card_number.toString().includes(Object.values({ filter }));
+  //  })
+  //);
+
+  const filteredCardData = useMemo(
+    () =>
+      cardData.filter((card) => card.card_number.toString().includes(filter)),
+    [cardData, filter]
   );
 
   const renderCardData = (
     <div className="overflow-x-auto">
-      <table className="table w-full">
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Bit Length</th>
-            <th>Region Code</th>
-            <th>Facility Code</th>
-            <th>Card Number</th>
-            <th>Issue Level</th>
-            <th>Hex</th>
-            <th>Raw</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredCardData.map((item, index) => (
-            <tr key={index}>
-              <td>{renderCardTypeImage(item.card_type)}</td>
-              <td>{item.bit_length}</td>
-              <td>{item.region_code}</td>
-              <td>{item.facility_code}</td>
-              <td>{item.card_number}</td>
-              <td>{item.issue_level}</td>
-              <td className="font-mono">{item.hex}</td>
-              <td>
-              <label htmlFor={("rawdata_modal_" + index)} className="btn btn-sm btn-outline btn-info">Show</label>
-              <input type="checkbox" id={("rawdata_modal_" + index)} className="modal-toggle" />
-              <div className="modal">
-                <div className="modal-box">
-                  <h3 className="text-lg font-bold">Raw Data</h3>
-                  <p className="py-4 font-mono">{item.raw}</p>
-                </div>
-                <label className="modal-backdrop" htmlFor={("rawdata_modal_" + index)}>Close</label>
-              </div>
-              </td>
+      {filteredCardData.length > 0 ? (
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>Bit Length</th>
+              <th>Region Code</th>
+              <th>Facility Code</th>
+              <th>Card Number</th>
+              <th>Issue Level</th>
+              <th>Hex</th>
+              <th>Raw</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredCardData.map((item, index) => (
+              <tr key={index}>
+                <td>{renderCardTypeImage(item.card_type)}</td>
+                <td>{item.bit_length}</td>
+                <td>{item.region_code}</td>
+                <td>{item.facility_code}</td>
+                <td>{item.card_number}</td>
+                <td>{item.issue_level}</td>
+                <td className="font-mono">{item.hex}</td>
+                <td>
+                  <label
+                    htmlFor={"rawdata_modal_" + index}
+                    className="btn btn-sm btn-outline btn-info"
+                  >
+                    Show
+                  </label>
+                  <input
+                    type="checkbox"
+                    id={"rawdata_modal_" + index}
+                    className="modal-toggle"
+                  />
+                  <div className="modal">
+                    <div className="modal-box">
+                      <h3 className="text-lg font-bold">Raw Data</h3>
+                      <p className="py-4 font-mono">{item.raw}</p>
+                    </div>
+                    <label
+                      className="modal-backdrop"
+                      htmlFor={"rawdata_modal_" + index}
+                    >
+                      Close
+                    </label>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className="flex justify-center items-center pt-6">
+          <div className="text-center">
+            <div className="alert alert-info">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="stroke-current shrink-0 w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+              <span>No card data found.</span>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 
@@ -137,8 +193,7 @@ export default function DataTable({ filter }) {
 
   return (
     <div className="container mx-auto pt-4">
-      {isLoading && <Spinner />}
-      {cardData.length > 0 ? renderCardData : renderNoCardData}
+      {isLoading ? <Spinner /> : renderCardData}
       {error && <ErrorAlert message={error} />}
     </div>
   );
